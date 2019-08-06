c25e8d926ea225b136be08c6cf85bc396acd3cba
Merelvanpuymbroeck <Merelvanpuymbroeck@gmail.com>
2019-08-06 09:53:46 +0200

    Error fields

--- forum.php
+++ forum.php
@@ -1,9 +1,26 @@
-<?php 
-$name = $_POST "name";
-$email = $_POST "email";
-$message = $_POST "message";
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $messagerErr = "";
 
-$bag = $_POST[];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
 
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
 
+  if (empty($_POST["message"])) {
+    $messagerErr = "a message is requierd";
+  } else {
+    $messagerErr = test_input($_POST["message"]);
+  }
+}
+
+// the error messages
 ?>
